77ebf5c0b4516322a6c35c89aa78861515a03e20
pipa0309 <melatonin24@yandex.ru>
2021-11-15 18:23:42 +0300

    forCheckTask5.1

---
+++ Person.java
@@ -0,0 +1,71 @@
+package HomeTaskFive;
+
+public class Person {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Person(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfoPerson() {
+        System.out.printf(" Name: %s\n Position: %s\n Email: %s\n Phone number: %s\n Salary: %d\n Age: %d\n\n", fullName, position, email, phoneNumber, salary, age);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
